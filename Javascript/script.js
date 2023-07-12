3d9fa44ed236809862c47d914f3518049082de69
INCONSOLATA00 <gabe.rattigan@gmail.com>
2023-07-12 14:50:08 -0400

    changed the location of function braces

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,40 +1,44 @@
 // getPlayerChoice (fixed)
 
-const getPlayerChoice = prompt('type, either; rock, paper or scissors')
+const getPlayerChoice = prompt('type either, rock, paper, or scissors')
 
 // getComputerChoice (random)
 
 function getComputerChoice() {
     randomNumber = Math.ceil(Math.random() * 3);
-}
+
 
 if (randomNumber == 1) {
-    return "rock";
+    return 'rock';
 } else if (randomNumber == 2) {
-    return "paper";
+    return 'paper';
 } else {
-    return "scissors";
+    return 'scissors';
+}
+
 }
 
 // Play single round (list all scenario where the computer wins first to
 // eliminate all variables)
 
 function singleRound(getPlayerChoice, getComputerChoice) {
-}
+
 if (getPlayerChoice == getComputerChoice) {
-    return 'Draw'
+    return 'Draw';
 }
 else if (getPlayerChoice == 'rock' && getComputerChoice == 'paper') {
-    return 'Computer Wins'
+    return 'Computer Wins';
 }
 else if (getPlayerChoice == 'scissors' && getComputerChoice == 'rock') {
-    return 'Computer Wins'
+    return 'Computer Wins';
 }
 else if (getPlayerChoice == 'paper' && getComputerChoice == 'scissors') {
-    return 'Computer Wins'
+    return 'Computer Wins';
+} else {
+    return 'Player Wins';
 }
 
-
+}
 
 getComputerChoice();
 
