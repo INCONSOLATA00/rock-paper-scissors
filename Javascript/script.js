e48a4db60d85ba78261c3f3b0bd8747e6ac99ba4
INCONSOLATA00 <gabe.rattigan@gmail.com>
2023-11-19 06:07:49 -0500

    added button functionality

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,33 +1,38 @@
-let getPlayerChoice = (button) => {
-return button;
+let computerAns;
+let getPlayerChoice = (playerChosenButton) => {
+    
+    console.log(playerChosenButton);
+    getComputerChoice();
+        function getComputerChoice() {
+
+            randomNumber = Math.ceil(Math.random() * 3);
+
+            if (randomNumber == 1) {
+                computerAns = 'rock';
+            } else if (randomNumber == 2) {
+                computerAns = 'paper';
+            } else {
+                computerAns = 'scissors';
+            }
+                return  singleRound(playerChosenButton, computerAns);
+        }
+
+    function singleRound(playerChosenButton, computerAns) {
+
+        if (playerChosenButton == computerAns) {
+            console.log('Draw');
+        } else if (playerChosenButton == 'rock' && computerAns == 'paper') {
+            console.log('Computer Wins');
+        } else if (playerChosenButton == 'scissors' && computerAns == 'rock') {
+            console.log('Computer Wins');
+        } else if (playerChosenButton == 'paper' && computerAns == 'scissors') {
+            console.log('Computer Wins');
+        } else {
+            console.log('Player Wins');
+        }
+    }
 };
 
 
-function getComputerChoice() {
-    randomNumber = Math.ceil(Math.random() * 3);
-
-    if (randomNumber == 1) {
-        return 'rock';
-    } else if (randomNumber == 2) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    }
-}
-function singleRound(getPlayerChoice, getComputerChoice) {
-
-    if (getPlayerChoice == getComputerChoice) {
-        return 'Draw';
-    } else if (getPlayerChoice == 'rock' && getComputerChoice == 'paper') {
-        return 'Computer Wins';
-    } else if (getPlayerChoice == 'scissors' && getComputerChoice == 'rock') {
-        return 'Computer Wins';
-    } else if (getPlayerChoice == 'paper' && getComputerChoice == 'scissors') {
-        return 'Computer Wins';
-    } else {
-        return 'Player Wins';
-    }
-}
-console.log(singleRound(getPlayerChoice, getComputerChoice));
 
 // RUN: single round 5 times; keep score / log, determine the higher number as winner.
