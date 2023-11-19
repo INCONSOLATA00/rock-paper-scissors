9159ea8de7ca430b8aa09266ee94a0211a7bdc35
INCONSOLATA00 <gabe.rattigan@gmail.com>
2023-11-19 03:24:16 -0500

    included string conversion

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,43 +1,37 @@
-
-
-const getPlayerChoice = prompt('type either, rock, paper, or scissors')
-const computerSelection = getComputerChoice();
-
-
+let getPlayerChoice = prompt('type either, rock, paper, or scissors');
+const playerResponseConversion = getPlayerChoice.toLowerCase();
+console.log(`convert string to lower case:  ${playerResponseConversion}`);
 
 function getComputerChoice() {
     randomNumber = Math.ceil(Math.random() * 3);
 
 
-if (randomNumber == 1) {
-    return 'rock';
-} else if (randomNumber == 2) {
-    return 'paper';
-} else {
-    return 'scissors';
-}
+    if (randomNumber == 1) {
+        return 'rock';
+    } else if (randomNumber == 2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
 
 }
-
-
 
 function singleRound(getPlayerChoice, getComputerChoice) {
 
-if (getPlayerChoice == getComputerChoice) {
-    return 'Draw';
-}
-else if (getPlayerChoice == 'rock' && getComputerChoice == 'paper') {
-    return 'Computer Wins';
-}
-else if (getPlayerChoice == 'scissors' && getComputerChoice == 'rock') {
-    return 'Computer Wins';
-}
-else if (getPlayerChoice == 'paper' && getComputerChoice == 'scissors') {
-    return 'Computer Wins';
-} else {
-    return 'Player Wins';
-}
+    if (getPlayerChoice == getComputerChoice) {
+        return 'Draw';
+    } else if (getPlayerChoice == 'rock' && getComputerChoice == 'paper') {
+        return 'Computer Wins';
+    } else if (getPlayerChoice == 'scissors' && getComputerChoice == 'rock') {
+        return 'Computer Wins';
+    } else if (getPlayerChoice == 'paper' && getComputerChoice == 'scissors') {
+        return 'Computer Wins';
+    } else {
+        return 'Player Wins';
+    }
 
 }
 
-console.log(singleRound(getPlayerChoice, computerSelection));
+console.log(singleRound(getPlayerChoice, getComputerChoice));
+
+// RUN: single round 5 times; keep score / log, determine the higher number as winner.
